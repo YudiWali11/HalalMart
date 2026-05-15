6e3283bb694a3a0c3687397c19eb4d6cd8bb6ac3
YudiWaliyuddin <114556881+YudiWali11@users.noreply.github.com>
2026-05-15 14:13:22 +0700

    Fix Vercel request URI issue

--- api/index.php
+++ api/index.php
@@ -4,4 +4,9 @@
  * Laravel Vercel Entrypoint
  */
 
+if (isset($_GET['vercel_request_uri'])) {
+    $_SERVER['REQUEST_URI'] = $_GET['vercel_request_uri'];
+    unset($_GET['vercel_request_uri']);
+}
+
 require __DIR__ . '/../public/index.php';
